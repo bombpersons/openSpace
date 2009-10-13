2959e567ac59a1a08c5c7805ff8ef9dce9254573
Bombpersons <blindrabbits@gmail.com>
2009-10-13 19:35:01 +0100

    First commit. Getting the hang of using autoconf and automake.

---
+++ src/lib/openSpace.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+bool printInfo() {
+	cout << "Message from libOpenSpace =)\n";
+	
+	return true;
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <includes/openSpace.h>
+
+using namespace std;
+
+int main() {
+	cout << "Hello, this is a test =)" << "\n";
+	cout << "Looks like I can still write in C++, huh?" << "\n";
+	
+	printInfo();
+	
+	return 0;
+}
